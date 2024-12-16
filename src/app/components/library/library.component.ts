4011f0329f510a28abd24d446cfc32127e6aa84e
Cheikh-Tidiane1 <cheikh951812@gmail.com>
2024-12-16 20:34:41 +0100

    new component smallCard && display on library

--- src/app/components/library/library.component.ts
+++ src/app/components/library/library.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SmallSongCardComponent } from '../../shared/small-song-card/small-song-card.component';
+import { SongService } from '../../service/song.service';
+import { ReadSong } from '../../models/song.model';
 
 @Component({
   selector: 'app-library',
   standalone: true,
-  imports: [FontAwesomeModule,RouterModule],
+  imports: [FontAwesomeModule, RouterModule, SmallSongCardComponent],
   templateUrl: './library.component.html',
-  styleUrl: './library.component.scss'
+  styleUrl: './library.component.scss',
 })
-export class LibraryComponent {
+export class LibraryComponent implements OnInit {
+  songService = inject(SongService);
+  songs: Array<ReadSong> = [];
 
+  constructor() {
+    effect(() => {
+      if(this.songService.getAllSig().status === "OK") {
+        this.songs = this.songService.getAllSig().value!;
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    this.fetchSongs();
+  }
+
+  private fetchSongs() {
+    //this.isLoading = true;
+    this.songService.getAll();
+  }
 }
---
+++ src/app/shared/small-song-card/small-song-card.component.ts
@@ -0,0 +1,13 @@
+import { Component, input } from '@angular/core';
+import { ReadSong } from '../../models/song.model';
+
+@Component({
+  selector: 'app-small-song-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './small-song-card.component.html',
+  styleUrl: './small-song-card.component.scss'
+})
+export class SmallSongCardComponent {
+    song =  input.required<ReadSong>()
+}
